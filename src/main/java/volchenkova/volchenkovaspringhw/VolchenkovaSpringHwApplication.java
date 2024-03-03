e4590a13a17f975827f3a1b849668fcb64612c96
alena <misamistake@>
2024-03-03 21:12:36 +0300

    initial commit

---
+++ src/main/java/volchenkova/volchenkovaspringhw/VolchenkovaSpringHwApplication.java
@@ -0,0 +1,14 @@
+package volchenkova.volchenkovaspringhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolchenkovaSpringHwApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolchenkovaSpringHwApplication.class, args);
+	}
+
+}
