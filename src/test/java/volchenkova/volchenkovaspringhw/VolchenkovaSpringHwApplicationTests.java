e3ec98a4818904e5a754e6ddb0a8165985c39030
alena <misamistake@>
2024-03-03 21:12:13 +0300

    initial commit

---
+++ src/test/java/volchenkova/volchenkovaspringhw/VolchenkovaSpringHwApplicationTests.java
@@ -0,0 +1,13 @@
+package volchenkova.volchenkovaspringhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolchenkovaSpringHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
